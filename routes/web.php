ddf554643377e48356094be563974dd8cf0d5d20
syiroth <irothsyiroth@gmail.com>
2025-06-16 09:07:13 +0700

    install debugger

--- routes/web.php
+++ routes/web.php
@@ -1,2 +1,10 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/callback', function (Request $request) {
+  $data = $request->all();
+  $data['status'] = 'success';
+  return response()->json($data);
+});
