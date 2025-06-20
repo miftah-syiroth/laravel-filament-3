011836ce80f4e2f467fa38eb73564654f0a414ae
syiroth <irothsyiroth@gmail.com>
2025-06-20 22:13:28 +0700

    testing

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Spatie\Analytics\Facades\Analytics;
+use Spatie\Analytics\Period;
 
-Route::get('/callback', function (Request $request) {
-  $data = $request->all();
-  $data['status'] = 'success';
-  return response()->json($data);
+// Route::get('/callback', function (Request $request) {
+//   $data = $request->all();
+//   $data['status'] = 'success';
+//   return response()->json($data);
+// });
+
+
+Route::get('/analytics-test', function () {
+  $data = Analytics::fetchVisitorsAndPageViews(Period::days(7));
+  dd($data);
 });
