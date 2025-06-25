680852dd03b3ca3bc30cd91b4259176d2070f7fb
syiroth <irothsyiroth@gmail.com>
2025-06-25 22:56:16 +0700

    update authorize panel

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,3 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Spatie\Analytics\Facades\Analytics;
-use Spatie\Analytics\Period;
-use App\Http\Controllers\AuthDebugController;
-
-// Route::get('/callback', function (Request $request) {
-//   $data = $request->all();
-//   $data['status'] = 'success';
-//   return response()->json($data);
-// });
-
-Route::get('/debug-auth', [AuthDebugController::class, 'debugAuth']);
-Route::get('/test-oauth-callback', [AuthDebugController::class, 'testOAuthCallback']);
-
-Route::get('/analytics-test', function () {
-  $data = Analytics::fetchVisitorsAndPageViews(Period::days(7));
-  dd($data);
-});
