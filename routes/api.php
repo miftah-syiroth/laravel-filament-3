498cd10f378f0b4077e7b91b76f55b316daba764
syiroth <irothsyiroth@gmail.com>
2025-07-04 15:49:21 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/articles/search', [App\Http\Controllers\API\ArticleController::class, 'search']);
+Route::get('/articles/summary', [App\Http\Controllers\API\ArticleController::class, 'summary']);
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/articles/search', [App\Http\Controllers\API\ArticleController::class, 'search']);
-Route::get('/articles/summary', [App\Http\Controllers\API\ArticleController::class, 'summary']);
